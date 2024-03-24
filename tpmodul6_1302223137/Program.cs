74bdb1a29e2b56371075a91e1cc33e067d2d8412
unknown <frarsa18@gmail.com>
2024-03-24 23:55:30 +0700

    implementasi design by contract

--- tpmodul6_1302223137/Program.cs
+++ tpmodul6_1302223137/Program.cs
@@ -1,46 +1,84 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        SayaTubeVideo stv1 = new SayaTubeVideo("Tutorial Design By Contract – [Farah Amalia Putri]");
+     /*   SayaTubeVideo stv1 = new SayaTubeVideo("Tutorial Design By Contract – [Farah Amalia Putri]");
         stv1.IncreasePlayCount(180);
         stv1.PrintVideoDetails();
+     */
+
+        SayaTubeVideo stv1 = null;
+
+        //PreCondition
+        try
+        {
+            stv1 = new SayaTubeVideo("Tutorial Design By Contract – [Farah Amalia Putri]");
+            for (int j = 1; j <= 10000000; j++)
+            {
+                stv1.IncreasePlayCount(1);
+            }
+            stv1.PrintVideoDetails();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR! " + ex.Message);
+        }
+      
     }
 }
 
 public class SayaTubeVideo
 {
-        private int id;
-        private string title;
-        private int playCount;
+    private int id;
+    private string title;
+    private int playCount;
 
-        public SayaTubeVideo(string title)
+    public SayaTubeVideo(string title)
+    {
+        Contract.Requires(title.Length <= 100 && title != null); //Prekondisi
+        this.title = title;
+        this.id = Random();
+        this.playCount = 0;
+    }
+    private int Random()
+    {
+        Random random = new Random();
+        return random.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount(int pc)
+    {
+        Contract.Requires(pc > 0 && pc <= 10000000); //Prekondisi
+        Contract.Ensures(pc < int.MaxValue, "Error"); //Postkondisi
+
+        try //Ketika kondisi benar
         {
-            this.title = title;
-            this.id = Random();
-            this.playCount = 0;
+            checked
+            {
+                playCount += pc;
+            }
+
         }
-        private int Random()
+        catch (OverflowException) //Ketika kondisi salah
         {
-            Random random = new Random();
-            return random.Next(10000, 99999);
+            Console.WriteLine("ERROR! Melebihi Kapasitas");
         }
 
-        public void IncreasePlayCount(int pc)
-        {
-            playCount += pc;
-        }
+    }
 
-        public void PrintVideoDetails()
-        {
-            Console.Write("ID: " + id);
-            Console.WriteLine(" ");
-            Console.Write("Title: " + title);
-            Console.WriteLine(" ");
-            Console.Write("Jumlah putar: " + playCount);
-        }
+    public void PrintVideoDetails()
+    {
+        Console.Write("ID: " + id);
+        Console.WriteLine(" ");
+        Console.Write("Title: " + title);
+        Console.WriteLine(" ");
+        Console.Write("Jumlah putar: " + playCount);
+    }
 }
 
--- tpmodul6_1302223137/obj/Debug/net8.0/tpmodul6_1302223137.AssemblyInfo.cs
+++ tpmodul6_1302223137/obj/Debug/net8.0/tpmodul6_1302223137.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_1302223137")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbeab1d1a05932d615252d42e58d68fff21ba93f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e18df416d13eb1c6926ab3ab13fd6a621a5ea5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_1302223137")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_1302223137")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
